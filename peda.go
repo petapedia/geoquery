364b3df8e3cfaadf9757ef16c758e0298a3b409f
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-09 19:13:58 +0700

    add GCF peda

--- peda.go
+++ peda.go
@@ -1,1 +1,16 @@
 package peda
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func GCFPeda(w http.ResponseWriter, r *http.Request) {
+	mconn := SetConnection("MONGOULBI", "petapedia")
+	datagedung := GetAllBangunanLineString(mconn, "petapedia")
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(datagedung)
+
+	//fmt.Fprintf(w, datagedung)
+}
