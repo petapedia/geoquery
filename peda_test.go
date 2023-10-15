32a4e84d700839de0d7dba13eee4d05da8df0116
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-15 10:40:22 +0700

    generate hash bcrypt

--- peda_test.go
+++ peda_test.go
@@ -13,6 +13,16 @@ func TestUpdateGetData(t *testing.T) {
 	fmt.Println(datagedung)
 }
 
+func TestGeneratePasswordHash(t *testing.T) {
+	password := "secret"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
+}
 func TestGeneratePrivateKeyPaseto(t *testing.T) {
 	privateKey, publicKey := watoken.GenerateKey()
 	fmt.Println(privateKey)
