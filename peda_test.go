7672fd63936b68a789a91c85f00f20af4613cc4e
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-15 10:22:49 +0700

    generate paseto

--- peda_test.go
+++ peda_test.go
@@ -3,10 +3,18 @@ package peda
 import (
 	"fmt"
 	"testing"
+
+	"github.com/whatsauth/watoken"
 )
 
 func TestUpdateGetData(t *testing.T) {
 	mconn := SetConnection("MONGOULBI", "petapedia")
 	datagedung := GetAllBangunanLineString(mconn, "petapedia")
 	fmt.Println(datagedung)
+}
+
+func TestGeneratePrivateKeyPaseto(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	fmt.Println(privateKey)
+	fmt.Println(publicKey)
 }
